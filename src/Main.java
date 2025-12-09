411dd1c6ffe7bca3bdf75c93516ca7198a16ed5f
Thiago <thigstudy@gmail.com>
2025-12-08 21:44:17 -0300

    chore: Clean up Main class by removing test execution code.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        ITaskDAO taskDAO = new TaskDAO();
 
-        taskDAO.completeTaskById(1);
     }
 }
